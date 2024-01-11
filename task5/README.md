7ab82aa79b2f112559a7e1f2490b4b4729a09324
dimabodriagin <65782955+dimabodriagin@users.noreply.github.com>
2024-01-11 11:06:28 +0400

    Add files via upload

---
+++ task5/README.md
@@ -0,0 +1,2 @@
+Для запуска сервера  json-server -p 3001 --watch server/db.json
+Для запуска фронта  npm start
---
+++ task5/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Фильмотека</h1>
+      <button className="header__button">Жарков Кирилл</button>
+    </header>
+  );
+};
+
+export default Header;
